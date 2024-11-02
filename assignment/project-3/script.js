5b8217519a6eda810939c65e1a9422da22b6d334
a9irudhh <01fe22bcs264@kletech.ac.in>
2024-11-02 23:20:20 +0530

    project 4 added

---
+++ assignment/project-3/script.js
@@ -0,0 +1,9 @@
+function handleLogin(event) {
+    event.preventDefault();
+    window.location.href = "form.html";
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    window.location.href = "thankyou.html";
+}
